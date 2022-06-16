115f1b13df8424c50dfa8c3fabb36b550d247b7f
Vimal Solanki <vimal.solanki@hotmail.co.uk>
2022-06-16 17:01:15 +0100

    class

---
+++ Register/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Register
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Area { get; set; }
+        public int PhoneNumber { get; set; }
+
+
+
+    }
+}
